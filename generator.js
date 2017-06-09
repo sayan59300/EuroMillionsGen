51258f032cd5f6ff2f1ae26b612b228d4afa88f7
Sayan593 <concepteur-developpeur@nicolas-buffart.fr>
2017-06-09 19:42:51 +0200

    update commentaires

--- generator.js
+++ generator.js
@@ -9,7 +9,6 @@ module.exports = {
     for (i=0; i<5; i++) {
       do {
         tirage = number.random(1,50);
-        //équivaut à => tirage = Math.floor((Math.random() * 50) + 1);
       } while (numbers.indexOf(tirage) !== -1);
       numbers.push(tirage);
     }
@@ -20,11 +19,9 @@ module.exports = {
     for (i=0; i<2; i++) {
       do {
         tirage = number.random(1,12);
-        //équivaut à => tirage = Math.floor((Math.random() * 12) + 1);
       } while (bonus.indexOf(tirage) !== -1);
       bonus.push(tirage);
     }
     return bonus;
-    //équivaut à => bonus = Math.floor((Math.random() * 10) + 1);
   }
 };
